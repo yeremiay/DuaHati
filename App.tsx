0e17c5eed54b603d61793469e30d2342519c6415
yeremiay <yeremia.artemis@gmail.com>
2026-03-14 10:01:39 +0700

    feat: safe-area-view loginscreen and text input only number

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import LoginScreen from './src/LoginScreen'; // Manggil file yang baru dibuat
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { View } from 'react-native';
 
 const App = () => {
   // Sekarang App cuma bertugas nampilin LoginScreen
-  return <LoginScreen />;
+  // return <LoginScreen />;
+  // Tambahan safeareaprovider biar gak nyenggol notch
+  return (
+    // <SafeAreaProvider>
+    //   <LoginScreen />
+    // </SafeAreaProvider>
+    <SafeAreaProvider>
+        <LoginScreen />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/FontAwesome'; // Pakai ikon hati yang tadi sukses!
 
 const LoginScreen = () => {
@@ -19,29 +20,39 @@ const LoginScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      {/* Bagian Judul */}
-      <View style={styles.header}>
-        <Text style={styles.title}>DuaHati</Text>
-        <Icon name="heart" size={24} color="#ff3b30" style={styles.icon} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        {/* Bagian Judul */}
+        <View style={styles.header}>
+          <Text style={styles.title}>DuaHati</Text>
+          <Icon name="heart" size={24} color="#ff3b30" style={styles.icon} />
+        </View>
+        <Text style={styles.subtitle}>Masukkin nomor HP kamu</Text>
+
+        {/* Kolom Input Nomor HP */}
+        <TextInput
+          style={styles.input}
+          placeholder="Contoh: +62812..."
+          placeholderTextColor="#999"
+          keyboardType="phone-pad" // Biar keyboard yang muncul langsung angka
+          value={phoneNumber}
+          onChangeText={phoneNumber => {
+            // Kita pakai rumus "Regex" untuk membuang semua yang BUKAN angka
+            // [^0-9] artinya: cari apapun yang bukan angka 0 sampai 9
+            // g artinya: cari di seluruh teks (global)
+            const cleaned = phoneNumber.replace(/[^0-9]/g, '');
+
+            // Baru kita simpan ke state
+            setPhoneNumber(cleaned);
+          }} // Simpan ketikan ke memory (state)
+        />
+
+        {/* Tombol Kirim */}
+        <TouchableOpacity style={styles.button} onPress={handleSendOTP}>
+          <Text style={styles.buttonText}>Kirim Kode OTP</Text>
+        </TouchableOpacity>
       </View>
-      <Text style={styles.subtitle}>Masuk pakai nomor HP kamu dulu ya</Text>
-
-      {/* Kolom Input Nomor HP */}
-      <TextInput
-        style={styles.input}
-        placeholder="Contoh: +62812..."
-        placeholderTextColor="#999"
-        keyboardType="phone-pad" // Biar keyboard yang muncul langsung angka
-        value={phoneNumber}
-        onChangeText={setPhoneNumber} // Simpan ketikan ke memory (state)
-      />
-
-      {/* Tombol Kirim */}
-      <TouchableOpacity style={styles.button} onPress={handleSendOTP}>
-        <Text style={styles.buttonText}>Kirim Kode OTP</Text>
-      </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -50,11 +61,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    // justifyContent: 'flex-start',
     padding: 20,
     backgroundColor: '#fff',
   },
   header: {
     flexDirection: 'row',
+    // flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 10,
